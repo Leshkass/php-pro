e49234542b40900ec5060673945e7de8a1495e11
leshkas <alassstttar@gmail.com>
2024-04-17 23:29:05 +0300

    Lesson 4

--- autoloader.php
+++ autoloader.php
@@ -1,6 +1,8 @@
 <?php
 
 spl_autoload_register(function ($className) {
+
     $classFilePath = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+
     require_once (__DIR__ . DIRECTORY_SEPARATOR . $classFilePath . '.php');
 });
