bf621932eec30b8559eb47609731aa4d44654296
dYackovchuk <d.yakovchuk@onixlab.com.ua>
2024-04-29 16:39:15 +0300

    change status code to 400, because this http code is usually used to display such an error

--- CarMaster/Exceptions/InvalidName.php
+++ CarMaster/Exceptions/InvalidName.php
@@ -6,7 +6,7 @@ use Exception;
 
 class InvalidName extends Exception
 {
-    public function __construct($message = "Invalid name", $code = 52)
+    public function __construct($message = "Invalid name", $code = 400)
     {
         parent::__construct($message, $code);
     }
