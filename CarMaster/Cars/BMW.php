0baa9c5135bf27a9f65ee4dc05bff6dde1f31ccb
leshkas <alassstttar@gmail.com>
2024-05-03 01:19:46 +0300

    database

--- CarMaster/Cars/BMW.php
+++ CarMaster/Cars/BMW.php
@@ -20,7 +20,7 @@ class BMW extends Car
      */
     public function setYear(int $year): void
     {
-        if ($year <= 1980) {
+        if ($year <= 1950) {
             throw new InvalidYearCar('Year must be more 1980');
         }
 
